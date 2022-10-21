0cf2d0a190ad5a2275e3c96e57348c853b55fc9b
LK <liukun1235@qq.com>
2022-10-21 10:25:41 +0800

    add shellSort 10/21

--- main.c
+++ main.c
@@ -100,10 +100,25 @@ void dualPivotQuickSort(int arr[],int start,int end){
     dualPivotQuickSort(arr,right + 1,end);
 }
 
+void shellSort(int arr[],int size){
+    int delta = size/2;
+    while (delta >= 1){
+        for (int i = delta; i < size; ++i) {
+            int j = i,tmp = arr[i];
+            while (j >= delta && arr[j-delta] > tmp){
+                arr[j] = arr[j - delta];
+                j -= delta;
+            }
+            arr[j] = tmp;
+        }
+        delta /= 2;
+    }
+}
+
 int main(){
     int arr[] = {3,5,7,2,9,0,6,1,8,4};
 
-    dualPivotQuickSort(arr,0,9);
+    shellSort(arr,10);
 
     for(int i = 0; i< 10; ++i){
         printf("%d ",arr[i]);
